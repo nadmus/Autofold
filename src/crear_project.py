2785d5b0a4b77e4eaaa4c8bf584b1de0b4fc62d5
Nadir Mustafa <mustafa.nadiragustin@gmail.com>
2026-04-27 19:55:31 -0300

    commit_1

--- src/crear_project.py
+++ src/crear_project.py
@@ -1,12 +1,32 @@
 from pathlib import Path
 import sys
+import subprocess
+import os
+from dotenv import load_dotenv 
+
+
+load_dotenv()
+
+base_path = os.getenv('BASE_PATH')
+
+if not base_path:
+    print("No se encontro la direccion deseada")
+    sys.exit(1)
+
+base = Path(base_path)
 
 nombre = sys.argv[1]
 
-ruta = Path.nombre
-
+ruta = base / nombre
 ruta.mkdir()
 print(f"Proyecto '{nombre}' Creado")
 
-#(ruta/"src").mkdir()
-#(ruta/"tests").mkdir()
+(ruta / "src").mkdir()
+(ruta / "tests").mkdir()
+(ruta / "src" / f"{nombre}.py").touch()
+
+subprocess.run(["git","init"], cwd=ruta)
+subprocess.run(["py","-m","venv","venv"], cwd=ruta)
+subprocess.run(["code","."],cwd=ruta)
+
+
